f201b4daa347b184f5ecf2a222b7473e59d0a9cf
JuanPablo2219 <jpmalla@sudamericano.edu.ec>
2024-06-30 21:48:10 -0500

    Initial commit with 4 projects
    first project - eureka
    second project - students
    third project - grades
    fourth project - gateway

---
+++ students/src/main/kotlin/com/example/Students/model/Student.kt
@@ -0,0 +1,28 @@
+package com.example.Students.model
+
+import jakarta.persistence.Column
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.Table
+import org.jetbrains.annotations.NotNull
+
+@Table(name = "student")
+@Entity
+class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(updatable = false)
+
+    var id: Long? = null
+
+    @NotNull
+    var full_Name: String? = null
+
+    @NotNull
+    var gender: String? = null
+
+    @NotNull
+    var email: String? = null
+}
---
+++ students/src/main/resources/db/migration/V1__init.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS student(
+    id SERIAL PRIMARY KEY,
+    full_name VARCHAR(100) NOT NULL,
+    gender VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE
+);
